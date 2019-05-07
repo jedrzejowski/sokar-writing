071c611b5ba95bf3c91fe3365c86fbd013e10160
Adam Jędrzejowski <adam@jedrzejowski.pl>
2019-05-08 00:22:18 +0200

    Dodanie przykładów GDCM

---
+++ text/biblioteki/gdcm-image.cpp
@@ -0,0 +1,42 @@
+#include ...
+
+int main() {
+
+    /* Tworzymy obiekt czytacza i wczytujemy plik */
+    gdcm::ImageReader ir;
+    ir.SetFileName("/path/to/file");
+    if (!ir.Read()) {
+        /* W przypadku wystąpienia błędu możemy go obsłużyć.
+           Klasa gdcm::ImageReader zwróci błąd gdy w pliku nie
+           będzie obrazu, bądź będzie w niespieranym formacie */
+        return 1;
+    }
+
+    /* Pobieramy obiekt obrazu */
+    const gdcm::Image &gimage = ir.GetImage();
+
+    /* Tworzymy bufor i zmieniamy jego wielkość. */
+    std::vector<char> imgbuffer;
+    imgbuffer.resize( gimage.GetBufferLength() );
+
+    /* Pobieramy wymiary obrazu */
+    const unsigned int* dimension = gimage.GetDimensions();
+    unsigned int dimX = dimension[0];
+    unsigned int dimY = dimension[1];
+
+    if (gimage.GetPhotometricInterpretation() == 
+        gdcm::PhotometricInterpretation::RGB)
+        std::cout << "Jest to obraz RGB" << std::endl;
+
+    if (gimage.GetPhotometricInterpretation() == 
+        gdcm::PhotometricInterpretation::MONOCHROME2)  
+        std::cout << "Jest to obraz monochromatyczny typu drugiego\n";
+
+    if (gimage.GetPixelFormat() == gdcm::PixelFormat::UINT8)
+        std::cout << "Jest to obraz monochromatyczny typu drugiego\n";
+
+    /* Oczywiście dalej można pobrać plik i zbiór elementów */
+
+    const gdcm::File &file = ir.GetFile();
+    const gdcm::DataSet &dataset = ir.GetDataSet();
+}
---
+++ text/biblioteki/gdcm-reader.cpp
@@ -0,0 +1,46 @@
+#include ...
+
+int main() {
+
+    /* Tworzymy obiekt czytacza i wczytujemy plik */
+    gdcm::Reader reader;
+    reader.SetFileName("/path/to/file");
+    if (!reader.Read()) {
+        /* W przypadku wystąpienia błędu możemy go obsłużyć */
+        return 1;
+    }
+
+    /* Pobieramy obiekt pliku */
+    const gdcm::File &file = reader.GetFile();
+
+    /* Pobieramy obiekt zbioru danych */
+    const gdcm::DataSet &dataset = file.GetDataSet();
+
+    /* Tworzymy pomocniczą klasę do konwertowania danych na std::string */
+    gdcm::StringFilter stringFilter;
+    stringFilter.SetFile(file);
+
+    /* Tworzymy pomocnicze obiekty znaczników */
+    const static gdcm::Tag
+            TagPatientName(0x0010, 0x0010),
+            TagWindowCenter(0x0028, 0x1050),
+            TagWindowWidth(0x0028, 0x1051);
+
+    /* Pobieramy tekst, jeżeli się znajduje w zbiorze */
+    if (dataset->FindDataElement(TagPatientName))
+        std::string name = stringFilter.GetString(TagPatientName);
+
+    
+    if (dataset->FindDataElement(TagWindowCenter)){
+        /* Pobieramy element ze zbiory danych */
+        const DataElement& ele = dataset->GetDataElement(tag);
+        /* Pobieramy 16-bitowego inta */
+        short center = ele.GetByteValue()->GetPointer();
+    }
+
+    if (dataset->FindDataElement(TagWindowWidth)){
+        const DataElement& ele = dataset->GetDataElement(tag);
+        short width = ele.GetByteValue()->GetPointer();
+    }
+
+}
